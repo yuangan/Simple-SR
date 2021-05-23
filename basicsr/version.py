203b6aa1a952e15de05c3b0df3d4635396d719a2
StOnEGiggity <tangtianqi09@gmail.com>
2021-05-23 23:31:49 +1000

    benchmark_mucan

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sat May 22 23:49:42 2021
-__version__ = '1.1.0+2849a48'
+# TIME: Sun May 23 22:18:26 2021
+__version__ = '1.1.0+fda8f66'
 short_version = '1.1.0'
 version_info = (1, 1, 0)
